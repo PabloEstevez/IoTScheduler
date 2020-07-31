cdd266d9c5607185993ff876beb002c7a92272a3
PabloEstevez <pabloestevegonzalez@gmail.com>
2020-07-31 16:11:30 +0200

    gpio_status

--- DeviceController.py
+++ DeviceController.py
@@ -17,16 +17,14 @@ use_mqtt = False
 
 def on_off(id, status):
     if config["devices"][id]["type"] == "gpio":
-        gpio.setwarnings(False)
-        gpio.setmode(gpio.BCM)
-        gpio.setup(config["devices"][id]["pin"], gpio.OUT)
+        gpio_setup(id)
         if status == "true":
             gpio.output(config["devices"][id]["pin"],True)
             pass
         else:
             gpio.output(config["devices"][id]["pin"],False)
             pass
-        print(status)
+        gpio_status(id)
     elif config["devices"][id]["type"] == "i2c":
         print("I2C activated!")
 
@@ -34,6 +32,17 @@ def on_off(id, status):
         client = mqtt.Client()
         client.connect(config["mqtt"]["server"], config["mqtt"]["port"], config["mqtt"]["keepalive"])
         client.publish(id, status)
+
+
+def gpio_status(id):
+    gpio_setup(id)
+    print(gpio.input(config["devices"][id]["pin"]), end='')
+
+
+def gpio_setup(id):
+    gpio.setwarnings(False)
+    gpio.setmode(gpio.BCM)
+    gpio.setup(config["devices"][id]["pin"], gpio.OUT)
 ###
 
 
@@ -48,11 +57,16 @@ def on_off(id, status):
 def main():
     parser = argparse.ArgumentParser(description="IoTScheduler hardware controller.")
     parser.add_argument("--id", help="ID of the device to controll.", required=True)
-    parser.add_argument("--status", help="STATUS=[true/false] Set the device on/off.", required=True)
+    parser.add_argument("--set", help="SET=[true/false] Set the device on/off.", required=False)
+    parser.add_argument("--status", dest="status", action="store_true", help="ID of the device to controll.", required=False)
     args = parser.parse_args()
-    on_off(args.id, args.status)
+    global use_mqtt
+    use_mqtt = True
+    if args.set != None:
+        on_off(args.id, args.set)
+    if args.status:
+        gpio_status(args.id)
 
 if __name__ == "__main__":
-    use_mqtt = True
     main()
 
