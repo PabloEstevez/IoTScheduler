60765f1ba16a9a458e5e276896cbc15e83def43a
PabloEstevez <pabloestevegonzalez@gmail.com>
2021-03-12 18:38:43 +0100

    MQTT subscription improvement

--- MQTT_watcher.py
+++ MQTT_watcher.py
@@ -1,8 +1,21 @@
-from DeviceController import on_off
-import paho.mqtt.subscribe as subscribe
+from DeviceController import on_off,get_devices
+import paho.mqtt.client as mqtt
 
-def on_message_print(client, userdata, message):
+def callback_controller(client, userdata, message):
     print("%s %s" % (message.topic, message.payload.decode("utf-8")))
-    on_off("motor",message.payload.decode("utf-8"))
+    on_off(message.topic,message.payload.decode("utf-8"))
 
-subscribe.callback(on_message_print, "test", hostname="riego.local")
+mqttc = mqtt.Client()
+
+# Add message callbacks that will only trigger on a specific subscription match.
+MQTT_TOPICS=[]
+for device in get_devices():
+    mqttc.message_callback_add(device, callback_controller)
+    MQTT_TOPICS.append((device,0))
+
+# Connec to MQTT Server and Subscribe to device topics
+mqttc.connect("riego.local", 1883, 60)
+mqttc.subscribe(MQTT_TOPICS)
+
+mqttc.loop_forever()
+
